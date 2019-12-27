5b3dabe65214d2ae8f672128686e6805ed09eece
AlexanderKaran <hello@alexanderkaran.com>
2019-12-27 20:13:24 +0800

    Added base form and base components for starter project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+// Custom Inputs
+import CustomInput from "./components/CustomInput";
+import CustomLabel from "./components/CustomLabel";
+import CustomButton from "./components/CustomButton";
+
+import "./App.css";
 
 function App() {
+  const [form, setForm] = useState({ firstName: "", lastName: "", email: "" });
+
+  function handleValueChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  function onSubmut(event) {
+    event.preventDefault();
+    window.alert(
+      `You submitted the following user
+
+      Name: ${form.firstName} ${form.lastName}
+      Email: ${form.email}
+      `
+    );
+    setForm({ firstName: "", lastName: "", email: "" });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form className="form-container" onSubmit={onSubmut}>
+          <CustomLabel htmlFor="userFirstName">First Name</CustomLabel>
+          <CustomInput
+            id="userFirstName"
+            name="firstName"
+            value={form.firstName}
+            onChange={handleValueChange}
+          />
+
+          <CustomLabel htmlFor="userLastName">Last Name</CustomLabel>
+          <CustomInput
+            id="userLastName"
+            name="lastName"
+            value={form.lastName}
+            onChange={handleValueChange}
+          />
+
+          <CustomLabel htmlFor="userEmail">Email</CustomLabel>
+          <CustomInput
+            id="userEmail"
+            name="email"
+            value={form.email}
+            onChange={handleValueChange}
+          />
+
+          <CustomButton type="submit">Submit</CustomButton>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/components/CustomInput/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./CustomInput.css";
+
+const CustomInput = props => {
+  return <input className="custom-input" {...props} />;
+};
+
+export default CustomInput;
